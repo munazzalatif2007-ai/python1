3082c9efae6ba321d9df6a16390f2e35391ce11d
munazzalatif2007-ai <munazzaansari2007@gmail.com>
2026-05-07 16:40:08 +0500

    base

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
-print("HEllo")
+
+var1 = int(input("Enter first variable"))
+var2 = int(input("Enter second variable"))
+#print(type(var1) , type(var2) , var1 , var2)
+#sum = var1 + var2
+print("Sum is= " , var1+var2)
+#prod = var1*var2
+print("Product is= " , var1*var2)
+#mod = var1%var2
+print("Remainder is = " , var1%var2)
